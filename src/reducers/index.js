df6f022bf82d7a68ac3c92671fa70e2895e4303a
pyguy12 <wuwarren7360@gmail.com>
2019-08-12 23:00:37 -0500

    Changed combine-reducers key value pairs to make more semantic sense.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,5 +2,5 @@ import { combineReducers } from 'redux';
 import SearchFormReducer from './SearchFormReducer';
 
 export default combineReducers({
-  searchQuery: SearchFormReducer
+  searchResults: SearchFormReducer
 });
