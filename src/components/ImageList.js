aa6bce70ffe255e42ede430320f90a2658390569
pyguy12 <wuwarren7360@gmail.com>
2019-08-13 20:38:44 -0500

    Made render function in ImageList to map over results and render a list of images.

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const ImageList = ({ listName }) => {
+const ImageList = ({ listName, searchResults }) => {
   const renderList = searchResults => {
-    return;
+    if (!searchResults) {
+      return null;
+    }
+    return searchResults.map(result => {
+      return (
+        <div key={result.id} className="image-card-container">
+          <img src={result.urls.thumb} alt={result.alt_description} />
+          <h2 className="image-card-user-name">{result.user.name}</h2>
+        </div>
+      );
+    });
   };
+
+  const imageList = renderList(searchResults);
 
   return (
     <div>
       <h1 className="list-name">{listName}</h1>
+      {imageList}
     </div>
   );
 };
