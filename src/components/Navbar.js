e79206c5a4879955c5316d463d3f2bbce31b7ffb
pyguy12 <wuwarren7360@gmail.com>
2019-08-16 23:37:28 -0500

    Added links back to / in Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './css/Navbar.css';
 
 const Navbar = () => {
   return (
     <nav className="navbar-container">
-      <i class="fas fa-home nav-button" />
-      <i class="fas fa-search nav-button" />
+      <Link className="nav-link" to="/">
+        <i class="fas fa-home nav-button" />
+      </Link>
+      <Link className="nav-link" to="/">
+        <i class="fas fa-search nav-button" />
+      </Link>
     </nav>
   );
 };
