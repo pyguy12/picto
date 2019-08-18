4977f629b3229b5dd6e09338af73c438a91e3a96
pyguy12 <wuwarren7360@gmail.com>
2019-08-18 15:05:47 -0500

    Added some components to SearchResults route.

--- src/components/routes/SearchResults.js
+++ src/components/routes/SearchResults.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ImageList from '../ImageList';
+import Hero from '../Hero';
+import SearchForm from '../SearchForm';
 
 const SearchResults = () => {
-  return <ImageList />;
+  return (
+    <React.Fragment>
+      <Hero
+        title="Picto"
+        description="Picto uses Unsplash’s developer API to retrieve images and was built
+        using React."
+        component={<SearchForm />}
+      />
+      <ImageList />
+    </React.Fragment>
+  );
 };
 
 export default SearchResults;
