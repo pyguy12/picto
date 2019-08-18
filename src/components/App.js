521f7cd6eaa7fa76b24e4864a41b7d4b8cd3cd38
pyguy12 <wuwarren7360@gmail.com>
2019-08-18 14:33:03 -0500

    Made SearchResults route to show results

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Navbar from './Navbar';
 import Home from './routes/Home';
 import './css/global.css';
 import MobileSearch from './routes/MobileSearch';
+import SearchResults from './routes/SearchResults';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Navbar />
         <Route path="/" exact component={Home} />
         <Route path="/mobile-search" component={MobileSearch} />
+        <Route path="/images/:id" component={SearchResults} />
       </div>
     </Router>
   );
---
+++ src/components/routes/SearchResults.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ImageList from '../ImageList';
+
+const SearchResults = () => {
+  return <ImageList />;
+};
+
+export default SearchResults;
