b975abe2a6751f1133c9ec84a6744a619e31dfed
pyguy12 <wuwarren7360@gmail.com>
2019-08-28 18:02:14 -0500

    Made some changes to make files ready for deployment to github pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './routes/Home';
 import MobileSearch from './routes/MobileSearch';
