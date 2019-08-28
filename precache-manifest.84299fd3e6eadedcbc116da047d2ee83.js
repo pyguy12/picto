49e1bace54548e0967f80c715d60800889bb7433
pyguy12 <wuwarren7360@gmail.com>
2019-08-28 18:01:11 -0500

    Updates

---
+++ precache-manifest.84299fd3e6eadedcbc116da047d2ee83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77456c42c464dce1193b31617dfa3284",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7feef4407fc6974a64c6",
+    "url": "/static/css/main.cdebffe9.chunk.css"
+  },
+  {
+    "revision": "8c1f4a754c6cff9fc3e6",
+    "url": "/static/js/2.d0a44041.chunk.js"
+  },
+  {
+    "revision": "7feef4407fc6974a64c6",
+    "url": "/static/js/main.3e24f11a.chunk.js"
+  },
+  {
+    "revision": "1c709190221a06e13395",
+    "url": "/static/js/runtime~main.d73c684b.js"
+  },
+  {
+    "revision": "ae56defe660812e548dd953ff294f264",
+    "url": "/static/media/heroImageLarge.ae56defe.jpg"
+  },
+  {
+    "revision": "09a41c5f95d132a877003213bda7d6e2",
+    "url": "/static/media/heroImageSmall.09a41c5f.jpg"
+  }
+]);
